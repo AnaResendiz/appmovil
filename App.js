5fc1d2310fe32b3e56f6beb9217c5d3a97e98df1
AnaResendiz <anaresendizser@gmail.com>
2020-02-26 11:33:21 -0600

    first commit

--- App.js
+++ App.js
@@ -1,24 +1,98 @@
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import Login from './screen/Login';
-import Registro from './screen/Registro';
+import React, { Component } from 'react';
+import {Container, Content, Card, CardItem, Text, Body, Button, Input, Item, Icon, Header} from 'native-base';
+import {StyleSheet, ScrollView} from 'react-native'
 
-const Stack = createStackNavigator();
-
-const App: () => React$Node = () => {
+class Login extends Component {
+  render(){
+    const navegar = this.props.navigation;
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'>
-        <Stack.Screen
-          name='Login'
-          component={Login}/>
-        <Stack.Screen
-          name='Registro'
-          component={Registro}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <> 
+      <Container>
+        <Header />
+        <ScrollView>
+        <Content padder 
+                  contentContainerStyle = {misEstilos.Content}>
+          <Card style = {misEstilos.textCenter} transparent>
+            <CardItem>
+              <Body>
+                <Text style = {misEstilos.textCenter}>
+                  Crea tu sesión.
+                </Text> 
+              </Body>
+            </CardItem>
+          </Card>
+          <Card style = {misEstilos.textCenter} transparent>
+            <CardItem>
+              <Body>
+                <Text style = {misEstilos.textCenter}>
+                Registrate con tu red social.
+                </Text> 
+              </Body>
+            </CardItem>
+          </Card>
+          <CardItem footer>
+                <Button secundary style={misEstilos.Boton3}><Icon type='Entypo' name='facebook'/></Button>
+                <Button info style={misEstilos.Boton2}><Icon type='AntDesign' name='twitter'/></Button>
+             </CardItem>
+          <Card>
+            <CardItem header bordered>
+              <Text style = {misEstilos.textCenter}>Registrate</Text>
+            </CardItem>
+            <CardItem bordered>
+              <Body style = {misEstilos.body}>
+              <Item inlineLabel>
+                <Icon type = 'FontAwesome' name = 'user'></Icon>
+              <Input placeholder = 'Nombre'/>
+            </Item>
+            <Item inlineLabel>
+                <Icon type = 'Zocial' name = 'gmail'></Icon>
+              <Input placeholder = 'Correo'/>
+            </Item>
+            <Item inlineLabel>
+                <Icon type = 'Entypo' name = 'tablet'></Icon>
+              <Input placeholder = 'Numero celular'/>
+            </Item>
+            <Item inlineLabel last>
+                <Icon type = 'FontAwesome' name = 'lock'></Icon>
+              <Input placeholder = 'Contraseña'/>  
+            </Item>
+              </Body>
+            </CardItem>
+            <CardItem footer bordered>
+              <Button primary onPress={() => navegar.navigate('Registro')} style = {misEstilos.boton}><Text> Entrar </Text></Button>
+            </CardItem>
+          </Card>
+        </Content>
+        </ScrollView>
+      </Container>
+    </>
   );
 };
+}
+
+const misEstilos = StyleSheet.create({
+  Content: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  textCenter:{
+    textAlign: 'center',
+    width: '100%',
+  },
+  boton:{ 
+    
+    marginLeft: '70%',
+  },
+  body:{
+    paddingVertical: 35,
+  },
+  Boton2:{
+    marginRight:'30%',
+  },
+  Boton3:{
+    marginLeft:'30%',
+  },
+
+});
 
 export default App;
